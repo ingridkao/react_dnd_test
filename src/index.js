44bc455629e151ffbd2d285e709dafc3e5f25494
ingridkao <ingrid7613@gmail.com>
2020-09-07 16:14:18 +0800

    verify that this subscription API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Container from './Container';
 import * as serviceWorker from './serviceWorker';
 
 import Board from './Board';
+import { observe } from './Game';
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <Board knightPosition={[7, 4]} />
-  // </React.StrictMode>
-  ,document.getElementById('root')
-);
+// ReactDOM.render(<Board knightPosition={[7, 4]} />,document.getElementById('root'));
+
+//透過Game裡面的observe方法讓棋子到處動
+observe((knightPosition) =>
+  ReactDOM.render(<Board knightPosition={knightPosition} />, document.getElementById('root'))
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
